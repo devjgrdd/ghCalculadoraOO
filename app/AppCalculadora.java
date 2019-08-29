e5479365452552db2e3f95a2a81cca67a80e1993
jesus portatil <jdevwd@gmail.com>
2019-08-29 19:42:52 +0200

    jgr-p - menu - creo menu()

--- app/AppCalculadora.java
+++ app/AppCalculadora.java
@@ -1,9 +1,18 @@
 package app;
 
 public class AppCalculadora {
+	
+	public static void menu() {
+		System.out.println("\nCALCULADORA");
+		System.out.println("sumar - s");
+		System.out.println("restar - r");
+		System.out.println("multiplicar - m");
+		System.out.println("dividir - d");
+		System.out.println("finalizar - f\n");
+	}
 
 	public static void main(String[] args) {
-		System.out.println("HOLA MUNDO");
+		
 	}
 
 }
